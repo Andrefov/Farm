e95150530867c9f5f60346064a155b7b8ed2f36b
Andrew Popiolek <andrew_1337@hotmail.com>
2022-03-29 21:22:59 +0200

    algorithm simplified

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -4,15 +4,7 @@ Sides RectangleSides::calculate(int x) {
 
 	int fance_amount = x;
 	int divided = x / 3;
-	switch (fance_amount % 3)
-	{
-	case 0:
-		return Sides(divided, divided);
-	case 1:
-		return Sides(divided, divided + 1);
-	case 2:
-		return Sides(divided, divided + 2);
-	}
+	return Sides(divided, divided + fance_amount % 3);
 };
 
 int RectAreaCalc::RectAreaCalculator(int a, int b) {
